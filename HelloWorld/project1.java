a59bbd7781c399c86db10f327fefb4e13a8f33be
Caleb Smith <caleb@calebsmith.net>
2016-09-14 10:05:19 -0400

    renamed project 1 for clarity

---
+++ HelloWorld/project1.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Project1 here.
+ * 
+ * @author Caleb Smith
+ * @version 0.0.1
+ */
+public class project1
+{
+    public static void main(String args[])
+    {
+        System.out.println("From: Bill Smith");
+        System.out.println("Address: Dell Computer, Bldg 13");
+        System.out.println("Date: April 12, 2005");
+        System.out.println("");
+        System.out.println("To: Jack Jones");
+        System.out.println("");
+        System.out.println("Message: Help! I'm trapped inside a computer!");
+    }
+}
