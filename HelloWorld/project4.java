dffc443a3f8d0fd623fef3dd8da245d18015cb77
Caleb Smith <caleb@calebsmith.net>
2016-09-16 10:02:09 -0400

    project 4

---
+++ HelloWorld/project4.java
@@ -0,0 +1,10 @@
+
+public class project4 {
+
+	public static void main(String[] args) {
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + (79 + 3 * (4 + 82 - 68) - 7 + 19));
+		System.out.println("(179 + 21 + 10) / 7 + 181 = " + ((179 + 21 + 10) / 7 + 181));
+		System.out.println("10389 * 56 * 11 + 2246 = " + (10389 * 56 * 11 + 2246));
+	}
+
+}
