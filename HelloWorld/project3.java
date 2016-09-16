dbcddf76c790ec3a4cd43631e5ac6e22463e82da
Caleb Smith <caleb@calebsmith.net>
2016-09-16 09:08:49 -0400

    moved to eclipse, added project 3

---
+++ HelloWorld/project3.java
@@ -0,0 +1,16 @@
+// Caleb Smith
+// v0.0.1
+public class project3 {
+
+	public static void main(String[] args) {
+		String s1 = "Allan Alda";
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck";
+		System.out.println(s1.substring(2, 7));
+		System.out.println(s2.substring(2, 7));
+		System.out.println(s3.substring(2, 9));
+		
+
+	}
+
+}
