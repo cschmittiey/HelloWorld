a81d6a8178500a7a47e4c7479e498797ef491358
Caleb Smith <caleb@calebsmith.net>
2016-09-22 10:37:08 -0400

    7b done

---
+++ HelloWorld/project7b.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class project7b {
+	public static void main(String[] args) 
+	{
+		Scanner reader = new Scanner(System.in);
+		System.out.println("What is your first name? ");
+		String firstName = reader.next();
+		System.out.println("What is your last name? ");
+		String lastName = reader.next();
+		System.out.println("Your full name is " + firstName + " " + lastName);
+		
+		
+	}
+}
