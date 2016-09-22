75526b7b61005f9de8ffb74eac2a8ab2c8291c64
Caleb Smith <caleb@calebsmith.net>
2016-09-22 10:21:47 -0400

    project 6 done

---
+++ HelloWorld/project6.java
@@ -0,0 +1,11 @@
+
+public class project6 {
+
+	public static void main(String[] args) {
+		double d1 = 3 * Math.PI * Math.sin(Math.toRadians(187.0)) + Math.abs(Math.cos(Math.toRadians(122.0)));
+		double d2 = Math.pow(14.72,3.801) + Math.log(72);
+		System.out.println("d1 = " + d1);
+		System.out.println("d2 = " + d2);
+	}
+
+}
