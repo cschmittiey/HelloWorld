638a4bc03856bea013f28474c928a4e819c4434e
Caleb Smith <caleb@calebsmith.net>
2016-09-22 10:23:35 -0400

    variable not being used in project 5

--- HelloWorld/project5.java
+++ HelloWorld/project5.java
@@ -4,7 +4,6 @@ public class project5 {
 
 	public static void main(String[] args) {
 		double d1 = 37.9;
-		double d2 = 1004.128;
 		int i1 = 12;
 		int i2 = 18;
 		p("Problem 1: " + ( 57.2 * ( i1 / i2) + 1));
