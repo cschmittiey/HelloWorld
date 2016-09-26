062c70af1928e4da1b7b92f0a481549844e649c8
Caleb Smith <caleb@calebsmith.net>
2016-09-26 09:25:08 -0400

    switched from println to print in 7b

--- HelloWorld/project7b.java
+++ HelloWorld/project7b.java
@@ -7,11 +7,11 @@ public class project7b {
 	public static void main(String[] args) 
 	{
 		Scanner reader = new Scanner(System.in);
-		System.out.println("What is your first name? ");
+		System.out.print("What is your first name? ");
 		String firstName = reader.next();
-		System.out.println("What is your last name? ");
+		System.out.print("What is your last name? ");
 		String lastName = reader.next();
-		System.out.println("Your full name is " + firstName + " " + lastName);
+		System.out.print("Your full name is " + firstName + " " + lastName);
 		
 		
 	}
