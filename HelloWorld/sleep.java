1ec5759456381b2d4190b7fc26c4649c94535577
Caleb Smith <caleb@calebsmith.net>
2016-10-04 10:37:41 -0400

    began sleep project -- result is 8 days off. not sure why

---
+++ HelloWorld/sleep.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+// Caleb Smith
+// v0.0.1
+public class sleep {
+
+	public static void main(String[] args) {
+		//Get birthdate
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please enter your birthdate: ");
+		System.out.print("Year: ");
+		int birthYear = reader.nextInt();
+		System.out.print("Month: ");
+		int birthMonth = reader.nextInt();
+		System.out.print("Day: ");
+		int birthDay = reader.nextInt();
+		
+		//Get current date
+		System.out.println("Please enter the current date: ");
+		System.out.print("Year: ");
+		int year = reader.nextInt();
+		System.out.print("Month: ");
+		int month = reader.nextInt();
+		System.out.print("Day: ");
+		int day = reader.nextInt();
+		
+		//Convert years and months to days
+		int totalYears = year - birthYear;
+		int yearDays = totalYears * 365;
+		int totalMonths = month - birthMonth;
+		int monthDays = totalMonths * 30;
+		int totalDays = yearDays + monthDays + (birthDay - day);
+		
+		System.out.println("You have been alive for " + totalDays + " days.");
+		
+	}
+
+}
