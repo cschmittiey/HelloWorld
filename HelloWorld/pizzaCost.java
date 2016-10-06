93d368651be8d525589fea6c4f402835275cabcf
Caleb Smith <caleb@calebsmith.net>
2016-10-06 09:56:36 -0400

    finished project Pizza Cost

---
+++ HelloWorld/pizzaCost.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// Caleb Smith
+// v0.0.1
+
+public class pizzaCost {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the Diameter of the pizza in inches: ");
+		double diam = reader.nextInt();
+		System.out.println("The cost of making the pizza is: " + ( ( 0.05 * (diam * diam) ) +  1.75 ) );
+
+	}
+}
